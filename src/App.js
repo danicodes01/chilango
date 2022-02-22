a9f2e5b6fa9d3d0b1c797647fab98a008850e013
Daniel <danoladesign@gmail.com>
2022-02-22 15:03:50 -0600

    yikes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   function getLang (lang) {
     i18n.changeLanguage(lang)
   }
-  //getting ready to deploy
+  //getting ready to deploy again
 
   useEffect(() =>{
     return fetch(`${process.env.REACT_APP_API_URL}/chilango`, {
